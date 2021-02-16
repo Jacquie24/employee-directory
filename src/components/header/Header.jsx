3772ec4aff7dc037b89c0793e66f2527def77345
Jacquie24 <jgcaption@yahoo.com>
2021-02-16 14:54:14 -0500

    Added red border to bottom of header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,15 +3,18 @@ import "./Header.css";
 
 const Header = () => {
     return (
+      <>
         
             <div className="jumbotron jumbotron-fluid">
   <div className="container">
     <h1 className="display-4">Employee Directory</h1>
     <p className="lead">Click on Name to filter alphabetically or use the search bar to narrow your results.</p>
-    <div className="row push-to-bottom"></div>
+
 
   </div>
 </div>
+<hr></hr>
+</>
         
     );
 };
