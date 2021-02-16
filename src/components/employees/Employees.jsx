49800a12db77bce2efb18f7dba979ac064a11db2
Jacquie24 <jgcaption@yahoo.com>
2021-02-16 07:32:07 -0500

    Hard coded employees table

--- src/components/employees/Employees.jsx
+++ src/components/employees/Employees.jsx
@@ -7,30 +7,29 @@ class Employees extends Component {
                 <table className="table">
   <thead>
     <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
+      <th scope="col">Image</th>
+      <th scope="col">Name</th>
+      <th scope="col">Phone</th>
+      <th scope="col">Email</th>
+      <th scope="col">DOB</th>
     </tr>
   </thead>
   <tbody>
     <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
+      <td><img src="https://www.placecage.com/50/50" alt="Nic Cage"></img></td>
+      <td>Nicolas Cage</td>
+      <td>(303) 410-9936</td>
+      <td>nic@niciscool.com</td>
+      <td>12-12-1966</td>
     </tr>
     <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
+      <td><img src="http://placekitten.com/g/50/50" alt="Cool Cat"></img></td>
+      <td>Cool Cat</td>
+      <td>(303) 410-9937</td>
+      <td>cat@niciscool.com</td>
+      <td>09-20-1988</td>
     </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td colspan="2">Larry the Bird</td>
-      <td>@twitter</td>
-    </tr>
+
   </tbody>
 </table>
             </div>
